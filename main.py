dea0dc4d9a1c2798d729e2acb8f95bef7cfb07e0
mat <matdallen@gmail.com>
2023-08-08 12:08:32 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key_path = "/home/mat/Documents/ProgramExperiments/secretAPIkey"
+MODEL = "gpt-3.5-turbo-16k-0613"
+
+REQUEST = "Howdy do neighbor!"
+
+create_draft = openai.ChatCompletion.create(
+        model = MODEL,
+        messages = [
+            {"role": "system", "content": """
+            You are a friendly assistant. Please help with this request.
+            """},
+            {"role": "user", "content": REQUEST}
+        ]
+    )
+
+
